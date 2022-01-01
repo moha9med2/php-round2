ffc817c1d0c9dc2810341dc9665ae63963d0cddd
unknown <moha9medsaeed@gmail.com>
2022-01-01 19:45:00 +0200

    pagination

--- 06-uploaded_file/index.php
+++ 06-uploaded_file/index.php
@@ -28,23 +28,47 @@
     $conn = mysqli_connect('localhost', 'root', '', 'db');
 
     if (isset($_FILES['image']['name'])) {
-        $image_name = uniqid ().$_FILES['image']['name'];
+        $image_name = uniqid() . $_FILES['image']['name'];
         $image_tmb_name = $_FILES['image']['tmp_name'];
         $query = "INSERT INTO images (name) values ('$image_name')";
         mysqli_query($conn, $query);
         // var_dump($_FILES['image']);
 
-        move_uploaded_file($image_tmb_name,"images/$image_name");
-
+        move_uploaded_file($image_tmb_name, "images/$image_name");
     }
 
 
     // echo mysqli_error($conn);
 
     $query_select = "SELECT * from images";
-
     $result = mysqli_query($conn, $query_select);
 
+
+
+    $total_items = mysqli_num_rows($result);
+    $item_in_page = 2;
+    $num_pages = ceil($total_items / $item_in_page);
+
+    if (isset($_GET['i'])) {
+        $i = $_GET['i'] + 1;
+        if ($_GET['i'] > $num_pages) {
+            $i = $num_pages;
+        }
+
+        if ($_GET['i'] < 0) {
+            $i = 1;
+        }
+    } else {
+        $i = 0;
+    }
+
+
+
+
+
+
+    $query_select_pag = "SELECT * from images limit $item_in_page offset $i";
+    $result_pag = mysqli_query($conn, $query_select_pag);
 
     ?>
 
@@ -52,12 +76,26 @@
 
     <div class="container p-5 m-5">
         <div class="row">
-            <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <?php while ($row = mysqli_fetch_assoc($result_pag)) { ?>
                 <div class="col-3 border rounded">
+                    <h3><?php echo $row['id'] ?></h3>
                     <img class="img-fluid" src="images/<?php echo $row['name'] ?>" alt="">
                 </div>
             <?php    } ?>
         </div>
+    </div>
+    <div class="d-flex justify-content-center mx-auto">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="index.php?i=<?php echo $_GET['i'] - 1 ?>">Previous</a></li>
+                <?php for ($i = 1; $i <= $num_pages; $i++) { ?>
+                    <li class="page-item">
+                        <a class="page-link" href="index.php?i=<?php echo $i ?>"><?php echo $i ?></a>
+                    </li>
+                <?php } ?>
+                <li class="page-item"><a class="page-link" href="index.php?i=<?php echo $_GET['i'] + 1 ?>">Next</a></li>
+            </ul>
+        </nav>
     </div>
 
 
