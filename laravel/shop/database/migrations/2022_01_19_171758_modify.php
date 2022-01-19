c72e3ac6db1d2d526400c1f5a59a2a77f4cd4871
unknown <moha9medsaeed@gmail.com>
2022-01-19 19:55:29 +0200

    shop

---
+++ laravel/shop/database/migrations/2022_01_19_171758_modify.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Modify extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('title',90)->change();
+            $table->dropColumn('image');
+            $table->string('photo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
